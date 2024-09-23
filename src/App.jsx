63cd19454b1b27ca64de8e021c9b2eb48f789093
Bhanu pratap Behera <bhanupratapbehera193@gmail.com>
2024-09-23 15:41:02 +0530

    complete

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import ButtonGradient from "./assets/svg/ButtonGradient";
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import Benifits from "./components/Benifits";
+import Collaboration from "./components/Collaboration";
+import Services from "./components/Services";
+import Pricing from "./components/Pricing";
+import RoadMap from "./components/RoadMap";
+import Footer from "./components/Footer";
+const App = () => {
+  return (
+    <>
+      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+        <Header />
+        <Hero />
+        <Benifits />
+        <Collaboration />
+        <Services />
+        <Pricing />
+        <RoadMap />
+        <Footer />
+      </div>
+      <ButtonGradient />
+    </>
+  );
+};
+export default App;
